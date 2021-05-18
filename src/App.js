da5f9c05d4d4a9bfa75296b5b7fc326f0c17f1da
Wagner <wagnerburgie@gmail.com>
2021-05-18 18:42:39 -0300

    Alteração App, categorias e Notas

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ class App extends Component {
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.notas.criarNota}
-          categorias={this.categorias.categorias} />
+        <FormularioCadastro criarNota={this.notas.adicionarNota.bind(this.notas)}
+          categorias={this.categorias} />
         <main className="conteudo-principal">
           <ListaDeCategorias
             adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
             categorias={this.categorias}
           />
           <ListaDeNotas
-            apagarNota={this.notas.deletarNota}
-            notas={this.notas.notas}
+            apagarNota={this.notas.deletarNota.bind(this.notas)}
+            notas={this.notas}
           />
         </main>
       </section>
